e6ef9dc19d5279f0448c8b57f013b2e1093aa6e8
rafaelprietocuriel <39804644+rafaelprietocuriel@users.noreply.github.com>
2019-07-22 15:38:01 -0700

    Add files via upload

--- SmoothConcentratedObservations.R
+++ SmoothConcentratedObservations.R
@@ -1,40 +1,44 @@
 #### Smooth functions evaluated in concentrated observations
 #### Description
 ####
-####     code created to run a smoothed function f over concentrated 
-####     but continuous events, with a mark, and returns the value of f
-####     evaluated over the marks and for some intervals
+####     code created to run a smoothed function SmoothW on a data set.
+####     The data contains at least two paired variables. The function runs
+####     over a continuous variable, t, with a mark, x, and for the values
+####     of x, returns the value of a function f evaluated over the marks
+####     and for some intervals.
 ####
 #### Usage
-#### SmoothW(data, fun, 
+#### SmoothW(DB, fun, 
 ####         bw = 256,
 ####         part = 0.2,
 ####         iter = 100, 
-####         bw = 0.1)
+####         extra = 0.1)
 ####
 #### Arguments
-#### data - data frame containing two numeric arguments:
-####            t = time  
-####            x = marks
+#### DB - a data frame containing two arguments:
+####            t = numeric, "time" or continuous variable which is the basis of the result  
+####            x = marks (maybe more than one dimension)
 #### fun - function to apply to the set x
 #### bw - number of observations to return
 #### part - a value between 0 and 1 which is the max length of the partition for refinement. 
 #### larger values of part return a coarser partition. Smaller values 
 #### take longer and give less smooth values
 #### iter - iterations of the code
+#### extra - an additional parameter which controls the extra width of the intervals.
+#### The parameter extra is the proportion that the interval is extended.
 
 
-
-
-f <- function(DB, fun,
+SmoothW <- function(DB, fun,
               bw = 256,
               part = 0.2,
-              iter = 100){
-  u <- range(DB$time)
+              iter = 100,
+              extra = 0.1){
+  u <- range(DB$t)
   l <- u[2] - u[1]
+  
   #### the points in which the function is evaluated
-  tCells <- seq(from = u[1] - .1*l, 
-                to = u[2] + .1*l, 
+  tCells <- seq(from = u[1] - extra*l, 
+                to = u[2] + extra*l, 
                 length.out = bw)
   
   #### the matrix has as basis only NA
@@ -53,7 +57,7 @@ f <- function(DB, fun,
       QM <- Part[k+1]
       
       #### observations inside the window
-      D <- DB$time >= Qm & DB$time  < QM
+      D <- DB$t >= Qm & DB$t  < QM
       
       #### cells inside the window
       InCell <- tCells >= Qm & tCells  < QM
@@ -73,46 +77,86 @@ f <- function(DB, fun,
 }
 
 
-
-
-### test set
+### Example 1
+### It constructs a sample dataset, concentrated over t
 n <- 200
 t <- runif(n)^8
 x <- 16 * t + rnorm(n)
-DB <- data.frame(x = x, time = t)
-
+DB <- data.frame(x = x, t = t)
 g <- function(DB){return(mean(DB$x))}
-Res <- f(DB, fun = g, 
-         part = 0.1,
-         bw = 1000, iter = 200)
-plot(DB$time, DB$x)
-points(Res$EvalTime, Res$ObsMean, type = "l", col = 3, lwd = 2)
-points(Res$EvalTime, Res$ObsMax, type = "l", col = 2)
-points(Res$EvalTime, Res$ObsMin, type = "l", col = 2)
-points(Res$EvalTime, Res$Obs05, type = "l", col = 2)
-points(Res$EvalTime, Res$Obs95, type = "l", col = 2)
+
+Result <- SmoothW(DB, fun = g,
+                  part = 0.1,
+                  bw = 1000,
+                  iter = 200,
+                  extra = 0.01)
+plot(DB$t, DB$x)
+polygon(c(Result$EvalTime, rev(Result$EvalTime)),
+        c(Result$ObsMin, rev(Result$ObsMax)), 
+        col = rgb(.1, .2, .7, .2))
+polygon(c(Result$EvalTime, rev(Result$EvalTime)),
+        c(Result$Obs05, rev(Result$Obs95)), 
+        col = rgb(.1, .2, .7, .4))
+points(Result$EvalTime, Result$ObsMean, 
+       type = "l", col = 2, lwd = 4)
 
 
-
-
-### test set 2
+### Example 2
 ### the function returns a regression coeffiicient for observations within range
-n <- 200
-t <- runif(n)^8
+n <- 1000
+t <- runif(n)^3
 x1 <- 16 * t + rnorm(n)
-x2 <- -8 * x1 + 2* rnorm(n)
+x2 <- -8 * x1 + 1* rnorm(n)
+### the function g gives us, in this case, the coefficient of a linear model
 g <- function(DB){ 
   m <- lm(DB$x2 ~ DB$x1)
   return(m$coefficients[2])
   }
-DB <- data.frame(x1 = x1, x2 = x2, time = t)
+DB <- data.frame(x1 = x1, x2 = x2, t = t)
+
+Result <- SmoothW(DB, fun = g,
+                  part = 0.4,
+                  bw =  100,
+                  iter = 200)
+plot(DB$x1, DB$x2)
+
+### notice that since the function g gives the coefficient of a linear model
+plot(Result$EvalTime, Result$ObsMean, 
+     ylim = c(-11, -5))
+polygon(c(Result$EvalTime, rev(Result$EvalTime)),
+        c(Result$ObsMin, rev(Result$ObsMax)), 
+        col = rgb(.1, .2, .7, .2))
+polygon(c(Result$EvalTime, rev(Result$EvalTime)),
+        c(Result$Obs05, rev(Result$Obs95)), 
+        col = rgb(.1, .2, .7, .4))
+points(Result$EvalTime, Result$ObsMean, 
+       type = "l", col = 2, lwd = 4)
 
 
-Res <- f(DB, fun = g)
-plot(DB$time, DB$x1)
-points(Res$EvalTime, Res$ObsMean, type = "l", col = 2, lwd = 2)
-points(Res$EvalTime, Res$ObsMax, type = "l", col = 2)
-points(Res$EvalTime, Res$ObsMin, type = "l", col = 2)
-points(Res$EvalTime, Res$Obs05, type = "l", col = 2)
-points(Res$EvalTime, Res$Obs95, type = "l", col = 2)
+### Example 3
+### This example uses the package the swiss dataset, described here:
+### https://stat.ethz.ch/R-manual/R-devel/library/datasets/html/swiss.html
+DB <- data.frame(t = swiss$Catholic,
+                 x = swiss$Education)
+g <- function(DB){
+  m <- lm(DB$x ~ DB$t)
+  return(m$coefficients[2])
+}
+
+Result <- SmoothW(DB, fun = g,
+                  part = 0.2,
+                  bw =  100,
+                  iter = 1000)
+
+### notice that since the function g gives the coefficient of a linear model
+plot(Result$EvalTime, Result$ObsMean, 
+     ylim = c(-9, 7))
+polygon(c(Result$EvalTime, rev(Result$EvalTime)),
+        c(Result$ObsMin, rev(Result$ObsMax)), 
+        col = rgb(.1, .2, .7, .2))
+polygon(c(Result$EvalTime, rev(Result$EvalTime)),
+        c(Result$Obs05, rev(Result$Obs95)), 
+        col = rgb(.1, .2, .7, .4))
+points(Result$EvalTime, Result$ObsMean, 
+       type = "l", col = 2, lwd = 4)
 
